aceda931334518c1882335d22127c7a2c94182b8
Salvo Bonanno <salvob.labs@gmail.com>
2021-11-28 11:58:11 +0100

    get() function test

--- test/greeting.js
+++ test/greeting.js
@@ -5,4 +5,15 @@ contract("Greeting", function () {
     await Greeting.deployed();
     return assert.isTrue(true);
   });
+
+  describe("get()", () => {
+    it("returns greeting value", async () => {
+      const greetingContract = await Greeting.deployed();
+      const expectedValue = "Hello World!"
+
+      const greetingValue = await greetingContract.get()
+
+      assert.equal(greetingValue, expectedValue, "greeting value matches")
+    })
+  })
 });
