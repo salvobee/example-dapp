34a186272e0256b57ca9083227a94073a1a19e69
Salvo Bonanno <salvob.labs@gmail.com>
2021-11-28 12:21:30 +0100

    set() function test passed

--- contracts/Greeting.sol
+++ contracts/Greeting.sol
@@ -4,7 +4,13 @@ pragma solidity ^0.8.0;
 
 contract Greeting {
 
+    string greeting = "Hello World!";
+
     function get() public view returns (string memory) {
-        return "Hello World!";
+        return greeting;
+    }
+
+    function set(string memory _greeting) public {
+        greeting = _greeting;
     }
 }
